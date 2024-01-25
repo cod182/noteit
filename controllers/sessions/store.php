a216d30b121f3af623f656c6f9f8d4027458cb75
Codie Stephens-Evans <codie18@gmail.com>
2024-01-25 15:21:07 +0000

    slight refactor to login controller

--- controllers/sessions/store.php
+++ controllers/sessions/store.php
@@ -28,24 +28,20 @@ $db = App::resolve(Database::class);
 // Find All in users where the email matches the email from post form
 $user = $db->query('SELECT * FROM users WHERE email = :email', ['email' => $email])->find();
 
-// If no user found
-if (!$user) {
-  $errors['general'] = 'Not account found, please check your details and try again.';
-  return view('sessions/create.view.php', ['errors' => $errors]);
+// If user found
+if ($user) {
+  // Have user, but need to check password provided
+  if (password_verify($user['password'], $password)) { // using php method, user password from DB, user password from form
+    // True
+    // Login user if credentials match
+    login($user);
+    // Redirect
+    header('Location: /');
+    exit();
+  };
 }
 
-// Have user, but need to check password provided
-if (password_verify($user['password'], $password)) { // using php method, user password from DB, user password from form
-  // True
-  // Login user if credentials match
-  login($user);
-  // Redirect
-  header('Location: /');
-  exit();
-};
-// Password validation failed
-$errors['password'] = 'No account found for email/password. Please try again';
-$errors['email'] = 'No account found for email/password. Please try again';
+// Password validation failed / no user found
 $errors['general'] = 'No account found for email/password. Please try again';
 
 return view('sessions/create.view.php', ['errors' => $errors]);
