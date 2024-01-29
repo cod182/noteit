18c03ab9ddfe7d193620aee50b4811302aeb772e
Codie Stephens-Evans <codie18@gmail.com>
2024-01-29 11:48:49 +0000

    added composer and autoloader

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use Core\Router;
+
 use Core\Session;
 use Core\ValidationException;
 
@@ -8,15 +8,9 @@ session_start();
 
 const BASE_PATH = __DIR__ . '/../';
 
+require(BASE_PATH . '/vendor/autoload.php');
+
 require BASE_PATH . 'Core/functions.php';
-
-// Autoloading functions
-spl_autoload_register(function ($class) {
-  $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-  require base_path("{$class}.php");
-});
-
 require base_path('bootstrap.php');
 
 // new router instance
